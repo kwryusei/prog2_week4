1c213f908e6439536f04d1d23aec401acb56869a
Kawahara Ryusei <e225701@ie.u-ryukyu.ac.jp>
2022-11-01 09:36:49 +0900

    1st commit

---
+++ src/演習2.java
@@ -0,0 +1,14 @@
+public class 演習2{
+
+    public class Chatbot{
+
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("main method runs");
+        String bot1;
+        
+        
+    }    
+}
